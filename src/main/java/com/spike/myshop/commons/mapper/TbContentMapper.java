3ac2b680f8899fca263f086a5a30f6c39ed04c23
pansy <526915701@qq.com>
2020-03-23 11:08:11 +0800

    init commit

--- src/main/java/com/spike/myshop/commons/mapper/TbContentMapper.java
+++ src/main/java/com/spike/myshop/commons/mapper/TbContentMapper.java
@@ -1,7 +1,10 @@
 package com.spike.myshop.commons.mapper;
 
 import com.spike.myshop.commons.domain.TbContent;
+import com.spike.myshop.commons.utils.RedisCache;
+import org.apache.ibatis.annotations.CacheNamespace;
 import tk.mybatis.mapper.MyMapper;
 
+@CacheNamespace(implementation = RedisCache.class)
 public interface TbContentMapper extends MyMapper<TbContent> {
 }
